c25bf6e07275f717dcc4fa723517065b6f85b422
rp <renpeng@foryou56.com>
2019-06-09 11:56:04 +0800

    哈哈2

--- app/src/main/java/com/pingan/education/mianshitest/MainActivity.java
+++ app/src/main/java/com/pingan/education/mianshitest/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
         );
         //哈哈
+        //哈哈2
         //okhttp 源码分析
         OkHttpClient okHttpClient =new OkHttpClient.Builder().cache(new Cache(new File("cache"),24*1024*1024)).build();
         OkHttpClient client = new OkHttpClient();
